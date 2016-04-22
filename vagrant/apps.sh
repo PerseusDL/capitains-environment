b9a30923f0c1e5701bdabc931a9e1959970a8b17
Bridget Almas <balmas@gmail.com>
2016-04-22 14:18:57 -0400

    adding some notes on the next steps to automate data updates

--- vagrant/apps.sh
+++ vagrant/apps.sh
@@ -10,3 +10,12 @@ PS="$(docker ps | grep capitainsenvironment_web | awk '{print $1}')"
 echo "Preprocessing cache on docker container $PS"
 sh preprocess.sh "$PS"
 
+# TODO
+# last step will be to add these steps to a cronjob to enable regular
+# update of the data and refresh of the cash:
+# sh download-perseus-csel.sh
+# sh unzip-corpora-local-volume.sh (need to enable overwrite)
+# PS="$(docker ps | grep capitainsenvironment_web | awk '{print $1}')"
+# sh flush.sh $PS
+# sh preprocess.sh $PS
+
